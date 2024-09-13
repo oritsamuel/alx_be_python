9fa7887d4dc9258da33e736d03f348d45c3e489b
Orit Samuel <oritsamuelseid@gmail.com>
2024-09-13 09:05:24 +0300

    basic python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 10
+number2 = 5
+print("addition of ", number1, " and ", number2," is ", number1+number2)
+print("subtracton of " ,number1, " and ", number2, " is ",number1-number2)
+print("mulitiplication of ",number1, " and ", number2, " is ", number1*number2)
