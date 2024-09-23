dfeb1ebe6d24141e7b8a2fc2644a8abbf7d38677
Orit Samuel <oritsamuelseid@gmail.com>
2024-09-23 21:32:52 +0300

    weather

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+user=input("What's the user like toay?  (sunny/rainy/cold): ")
+if user == "sunny":
+    print("Wear a t-shirt an sunlasses.")
+elif user == "rainy":
+    print("Don't forget your umberella and a raincoat")
+elif user == "cold":
+    print("Make sure to wear a warm coat and a scarf")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
