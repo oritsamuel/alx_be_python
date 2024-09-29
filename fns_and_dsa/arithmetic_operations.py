bb2863d7688e2fa2d432878edf730f8dbc45a6f6
Orit Samuel <oritsamuelseid@gmail.com>
2024-09-29 21:29:42 +0300

    func

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation():
+    num1=float(input("Enter the first number: "))
+    num2=float(input("Enter the second number: "))
+    operation=input("Enter the operation (add, subtract, multiply, divide)")
+    match operation:
+        case 'add':
+            print("Result: ", num1 +num2)
+        case 'subtract':
+            print("Result: ", num1 - num2)
+        case 'multiply':
+            print("Result: ", num1 * num2)
+        case 'divide':
+            print("Result: ", num1 / num2)
+    
+perform_operation()
