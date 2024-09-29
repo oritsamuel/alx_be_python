8f7249d6269f7e1c958301ec9d748e79401fc0b2
Orit Samuel <oritsamuelseid@gmail.com>
2024-09-29 21:44:37 +0300

    new fun

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,7 +1,5 @@
-def perform_operation():
-    num1=float(input("Enter the first number: "))
-    num2=float(input("Enter the second number: "))
-    operation=input("Enter the operation (add, subtract, multiply, divide)")
+def perform_operation(num1, num2, operation):
+    
     match operation:
         case 'add':
             print("Result: ", num1 +num2)
@@ -11,5 +9,20 @@ def perform_operation():
             print("Result: ", num1 * num2)
         case 'divide':
             print("Result: ", num1 / num2)
+
+
+from arithmetic_operations import perform_operation
+
+def main():
+    print("Arithmetic Operations")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+    result = perform_operation(num1, num2, operation)
+    print(f"Result: {result}")
+
+if __name__ == "__main__":
+    main()
     
-perform_operation(num1,num2, operation)
+
