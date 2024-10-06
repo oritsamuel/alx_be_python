e81fa03d47d583f34cc163583c98653317cba012
Orit Samuel <oritsamuelseid@gmail.com>
2024-10-06 21:19:33 +0300

    pattern

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,17 @@
-num = int(input("Enter the size of the pattern: "))
-while True:
-    for i in range(num):
-        print("*", end=" ")
+def main():
+  while True:
+    try:
+      size = int(input("Enter the size of the pattern: "))
+      if size <= 0:
+        print("Please enter a positive integer.")
+      else:
+        break
+    except ValueError:
+      print("Invalid input. Please enter a number.")
+  for i in range(size):
+    for j in range(size):
+      print("*", end="")
+    print() 
 
+if __name__ == "__main__":
+  main()
