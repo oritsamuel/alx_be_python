16aaecd04b11d0d1147e8fd13be721bf1663fc7b
Orit Samuel <oritsamuelseid@gmail.com>
2024-10-06 20:46:59 +0300

    library

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -2,7 +2,7 @@ class Book:
   def __init__(self, title, author):
     self.title = title
     self.author = author
-    self._is_checked_out = False
+    self._is_checked_out = False  
 
   def __str__(self):
     return f"{self.title} by {self.author}"
