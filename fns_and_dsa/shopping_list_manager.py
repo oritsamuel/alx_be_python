f37c87fc4f1819ce9e3923fe5335767a730c5503
Orit Samuel <oritsamuelseid@gmail.com>
2024-10-06 21:50:43 +0300

    shop

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,46 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            new_item = input("Enter item name: ")
+            shopping_list.append(new_item)
+            print(f"{new_item} added to the list!")
+
+        elif choice == '2':
+            
+            item_to_remove = input("Enter item name to remove: ")
+            if item_to_remove in shopping_list:
+                shopping_list.remove(item_to_remove)
+                print(f"{item_to_remove} removed from the list!")
+            else:
+                print(f"{item_to_remove} not found in the list.")
+
+        elif choice == '3':
+            if shopping_list:
+                print("Your shopping list:")
+                for item in shopping_list:
+                    print(f"- {item}")
+            else:
+                print("Your shopping list is currently empty.")
+
+        elif choice == '4':
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
